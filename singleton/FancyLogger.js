e74a48741f0789df94e5951ef22622275468a912
Daniel PC <daniel.barczynski@onet.pl>
2022-11-27 17:40:01 +0100

    update

---
+++ singleton/FancyLogger.js
@@ -0,0 +1,18 @@
+class FancyLogger {
+    constructor() {
+        this.logs = [];
+    }
+
+    log(message) {
+        this.logs.push(message);
+        console.log(`FANCY: ${message}`);
+    }
+
+    logCount() {
+        console.log(`${this.logs.length} logs`);
+    }
+}
+
+const fancyLogger = new FancyLogger();
+fancyLogger.log('1 file');
+fancyLogger.logCount();
